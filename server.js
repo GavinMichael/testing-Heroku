8fe178c92cff95d4eba2f760fbfdb410891de25b
Gavin Michael <gavin.michael.ftw@gmail.com>
2017-07-18 11:02:07 +0400

    Edit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.user(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
-
 app.listen(port);
